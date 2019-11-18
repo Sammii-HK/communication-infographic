e1a476cca8c13524f7eb76d03bbb96ae2b002b1d
Samantha Kellow <sammii.h@icloud.com>
2019-11-18 17:16:19 +0000

    local node.js server created to load json file

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('JS loaded')
+const http = require('http')
 
+const hostname = '127.0.0.1'
+const port = 8000
 
-  
-
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Hello World\n')
 })
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`)
+})
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   console.log('JS loaded')
+//
+//
+//
+//
+// })
