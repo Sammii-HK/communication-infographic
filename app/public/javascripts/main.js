e350e7d5ee12ce82d5f40013ad73633201893ff3
Samantha Kellow <sammii.h@icloud.com>
2019-11-19 22:12:42 +0000

    file update

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -3,16 +3,22 @@ console.log('JS loaded 🍇')
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded 🍓')
 
+  let content
+
   fetch('./data.json')
     .then(response => {
       return response.json()
     })
     .then(data => {
       // Work with JSON data here
-      console.log(data)
+      content = data
+      console.log('content:', content)
     })
     .catch(err => {
       // Do something for an error here
       console.log(`Error: ${err}`)
     })
+
+
+  document.getElementById('timeline')
 })
