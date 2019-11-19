6c143afc85a3a9064c6da45dbcf22a0ec9000d11
Samantha Kellow <sammii.h@icloud.com>
2019-11-19 21:32:59 +0000

    remove unecessary code

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -2,4 +2,39 @@ console.log('JS loaded 🍇')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded 🍓')
+
+  fetch('./data.json')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      // Work with JSON data here
+      console.log(data)
+    })
+    .catch(err => {
+      // Do something for an error here
+      console.log(`Error: ${err}`)
+    })
+
+  // let data
+
+  // $.ajax({
+  //   beforeSend: function(xhr) {
+  //     if (xhr.overrideMimeType) {
+  //       xhr.overrideMimeType('application/json')
+  //     }
+  //   }
+  // })
+  //
+  // function loadData() {
+  //   $.getJSON('./data.json')
+  //     .done(function(res) {
+  //       data = res
+  //       console.log(data)
+  //     }).fail(function(res) {
+  //       console.log(`Error: ${res}`)
+  //     })
+  // }
+  //
+  // loadData()
 })
