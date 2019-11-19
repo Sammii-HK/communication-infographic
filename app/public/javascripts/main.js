9758a4e7bc98c952ecff49ce35d1ccbfb743b07f
Samantha Kellow <sammii.h@icloud.com>
2019-11-19 22:41:11 +0000

    main.js file tidy

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -1,35 +1,38 @@
 console.log('JS loaded 🍇')
+const timeline = document.getElementById('timeline')
 
+let content
+let itemContainer
+
+fetch('./data.json')
+  .then(response => {
+    return response.json()
+  })
+  .then(data => {
+    // Work with JSON data here
+    content = data
+    console.log('content:', content)
+  })
+  .catch(err => {
+    // Do something for an error here
+    console.log(`Error: ${err}`)
+  })
+
+
+// CREATE ITEMS FROM JSON OBJECTS AND PUSH TO DOM
+function makeTimeline() {
+  console.log(timeline)
+  console.log(content)
+  content.map(item => {
+    console.log('item:', item)
+  })
+}
+
+
+// DOM CONTENT LOADED
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded 🍓')
 
-  let content
-  let itemContainer
-
-  fetch('./data.json')
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      // Work with JSON data here
-      content = data
-      console.log('content:', content)
-    })
-    .catch(err => {
-      // Do something for an error here
-      console.log(`Error: ${err}`)
-    })
-
-
-  const timeline = document.getElementById('timeline')
-  console.log(timeline)
-  console.log(content)
-
-  function makeTimeline() {
-    content.map(item => {
-      console.log('item:', item)
-    })
-  }
   makeTimeline()
 
 })
