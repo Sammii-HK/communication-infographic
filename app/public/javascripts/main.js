f5c4150a63e898f02da4f139758151ec84d7d381
Samantha Kellow <sammii.h@icloud.com>
2019-11-20 13:35:01 +0000

    reverted back to fetch 🙈 rearranged code, and used promises to resolve content undefined issue

--- app/public/javascripts/main.js
+++ app/public/javascripts/main.js
@@ -4,40 +4,26 @@ let content
 let timeline
 let itemContainer
 
-// REFERENCE CODE ================== From 
-// handleSubmit(e) {
-//     e.preventDefault()
-//
-//     axios.post('/api/login', this.state.data)
-//       .then(res => {
-//         Auth.setToken(res.data.token)
-//         Flash.setMessage('success', res.data.message)
-//         // set the favorites from user db profile into local storage
-//         Favorite.setFavorites(res.data.favorites)
-//         this.props.history.push('/profile')
-//       })
-//       .catch(err => this.setState({ errors: err.response.data.error }))
-//   }
-// =================================
-
-fetch('./data.json')
-  .then(response => {
-    return response.json()
-  })
-  .then(data => {
-    // Work with JSON data here
-    content = data
-    console.log('content:', content)
-  })
-  .catch(err => {
-    // Do something for an error here
-    console.log(`Error: ${err}`)
-  })
+function loadData() {
+  fetch('./data.json')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      // Work with JSON data here
+      content = data
+      makeTimeline()
+      console.log('content:', content)
+    })
+    .catch(err => {
+      // Do something for an error here
+      console.log(`Error: ${err}`)
+    })
+}
 
 
 // CREATE ITEMS FROM JSON OBJECTS AND PUSH TO DOM
 function makeTimeline() {
-
   console.log('timeline', timeline)
   console.log('content', content)
   content.map(item => {
@@ -51,8 +37,6 @@ document.addEventListener('DOMContentLoaded', () => {
   timeline = document.getElementById('timeline')
   console.log('DOMContentLoaded 🍓')
 
-  if (content) {
-    makeTimeline()
-  }
+  loadData()
 
 })
