015910f91993b27458af2c7b3c187b123ce686be
Sammii <sammii.h@icloud.com>
2019-12-11 16:50:23 +0000

    node_modules refresh

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
 const routes = require('./config/routes')
 const errorHandler = require('./lib/errorHandler')
-const { port, dbUri } = require('./config/environment')
+const port = require('./config/environment')
 require('dotenv').config()
 
 
 const app = express()
-
-mongoose.connect(dbUri)
 
 app.use(express.static(`${__dirname}/dist`))
 
