d10035da3a2602ca51c7aabc5ce450fd44849b77
Sammii <sammii.h@icloud.com>
2019-12-11 22:39:34 +0000

    working on data sort function

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,8 @@ class Home extends React.Component {
     this.state = {
       data: null
     }
+
+    this.sortedData = this.sortedData.bind(this)
   }
 
   getData() {
@@ -16,17 +18,32 @@ class Home extends React.Component {
       .catch(err => console.error(err))
   }
 
+  sortedData() {
+    return this.state.data.sort((a, b) => {
+      if (a.year === b.year) return 0
+      return a.year < b.year ? -1 : 1
+    })
+  }
+
   componentDidMount() {
     this.getData()
-    console.log('data', this.state.data)
+    // console.log('data', this.state.data)
   }
 
   render() {
-    console.log(this.state.data)
+    console.log('state data', this.state.data)
     if (!this.state.data) return <h1>Loading...</h1>
     return (
       <div className='container'>
         <h1 className='title'>Hello Universe!</h1>
+        <div id='timeline'>
+          {this.sortedData().map(item =>
+            <div key={item._id} id={item._id} className={item._id}>
+              <h5>{item.title}</h5>
+              <p>{item.year}</p>
+            </div>
+          )}
+        </div>
       </div>
     )
   }
