5e469d000454b8ed355c83a39758140bcfcf67ca
Sammii <sammii.h@icloud.com>
2019-12-11 18:30:57 +0000

    trying to load json data with axios

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,34 @@
 import React from 'react'
+import axios from 'axios'
 
-import { Link } from 'react-router-dom'
+class Home extends React.Component {
 
-const Home = () => {
-  return(
-    <div className='container'>
-      <h1 className='title'>Hello Universe!</h1>
-    </div>
-  )
+  constructor() {
+    super()
+
+    this.state = {
+      data: null
+    }
+    activeItem: null
+  }
+
+  getData() {
+    axios.get('./data.json')
+      .then(res => this.setState({ data: res.data }))
+      .catch(err => console.error(err))
+  }
+  componentDidMount() {
+    this.getData()
+  }
+
+  render() {
+    if (!this.state.data) return <h1>Loading...</h1>
+    return (
+      <div className='container'>
+        <h1 className='title'>Hello Universe!</h1>
+      </div>
+    )
+  }
 }
-
 
 export default Home
