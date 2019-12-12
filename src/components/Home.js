ff135c324b774aafcb17d861c3fabc1a290c1c9a
Sammii <sammii.h@icloud.com>
2019-12-12 12:51:49 +0000

    started bringing in vanilla JS fucntions for placement of items

--- src/components/Home.js
+++ src/components/Home.js
@@ -26,23 +26,44 @@ class Home extends React.Component {
     })
   }
 
+  yAxis() {
+    const data = this.state.data.data
+    const y = 10
+    // PUSH ALL ELEMENTS UP FOR AESTHETICS
+    const year = data.year
+    // PLACE ITEMS ON ITEMLINE USING YEAR VALUE AND VALUE SET FOR 'Y'
+    return year * y
+  }
+
   componentDidMount() {
     this.getData()
-    // console.log('data', this.state.data)
+  }
+
+  // MAKE POSITIVE OR NEGATIVE NUMBER
+  posOrNeg() {
+    return Math.random() < 0.5 ? -1 : 1
+  }
+
+  randomNumber() {
+    const width = document.getElementById('timeline').offsetWidth
+    // MAKE A RANDOM VALUE WHICH IS HALF OF TIMELINE WIDTH
+    let randomValue = Math.round(Math.random() * (width / 2) - 1)
+    // RANDOMLY MAKE THE VALUE POSITIVE OR NEGATIVE
+    randomValue = randomValue * this.posOrNeg()
+    return randomValue
   }
 
   render() {
     console.log('state data', this.state.data)
-    // console.log('state data', this.state.data._id)
     if (!this.state.data) return <h1>Loading...</h1>
     return (
       <div className='container'>
         <h1 className='title'>Hello Universe!</h1>
         <div id='timeline'>
           {this.sortedData().map((item, i) =>
-            <div key={i} className={item.category}>
+            <div key={i} className={item.category}
+              style={{ transform: `translate(${this.xAxis}px, ${this.yAxis()}px)` }}>
               <h5>{item.title}</h5>
-              <p>{item.year}</p>
             </div>
           )}
         </div>
