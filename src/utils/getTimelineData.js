08638c1f37767035b7354e7295e633cd1e7306a7
Sammii <kellow.sammii@gmail.com>
2025-01-15 19:31:08 +0000

    creating overlapCheck function

--- src/utils/getTimelineData.js
+++ src/utils/getTimelineData.js
@@ -1,41 +1,43 @@
 import data from '../data.json';
 
-  // overlapCheck() {
-  //   const timelineDOM = document.getElementById('timeline')
-  //   const width = timelineDOM.offsetWidth
-  //   // const paddingValue = 0
-  //   const paddingValue = width / 12
-  //   const timeline = this.state.timeline.map((currentItem, i) => {
-  //     // let xAxis = currentItem.xAxis
-  //     const withinTenYears = []
-  //
-  //     console.log('current:', currentItem.title, currentItem.year, currentItem.xAxis)
-  //
-  //     for (let comparableIndex = 0; comparableIndex <= i; comparableIndex ++) {
-  //
-  //       if ((this.state.timeline[comparableIndex].year + 10) >= currentItem.year) {
-  //         withinTenYears.push(this.state.timeline[comparableIndex].xAxis)
-  //       }
-  //     }
-  //     // REMOVE LAST ITEM AS IT IS ITSELF
-  //     withinTenYears.pop()
-  //     console.log('withinTenYears', withinTenYears)
-  //
-  //     // // MAP WITHIN TEN YEARS ARRAY
-  //     // while (withinTenYears.some(value => {
-  //     //   return currentItem.xAxis >= (value - paddingValue) && currentItem.xAxis <= (value + paddingValue)
-  //     // })
-  //     // ) {
-  //     //   // MAKE A NEW NUMBER
-  //     //   const newNumber = this.randomNumber()
-  //     //   console.log('new number', newNumber)
-  //     //   // SET XAXIS VALUE FOR USE LATER
-  //     //   currentItem.xAxis = newNumber
-  //     // }
-  //     return currentItem
-  //   })
-  //   this.setState({ timeline })
-  // }
+
+  const overlapCheck = (timelineData, minGap = 50, timelineWidth) => {
+    const adjustedData = [...timelineData];
+  const halfWidth = timelineWidth / 2;
+
+  // Loop through each item in the timeline
+  for (let i = 0; i < adjustedData.length; i++) {
+    for (let j = i + 1; j < adjustedData.length; j++) {
+      const itemA = adjustedData[i];
+      const itemB = adjustedData[j];
+
+      // Check if items are within 10 years of each other
+      if (Math.abs(itemA.year - itemB.year) <= 10) {
+        // Ensure their xAxis values are sufficiently apart
+        while (Math.abs(itemA.xAxis - itemB.xAxis) < minGap) {
+          // Adjust xAxis of the second item to reduce overlap
+          itemB.xAxis += minGap * (Math.random() < 0.5 ? -1 : 1);
+
+          // Ensure the xAxis stays within the bounds of the timelineWidth, with minGap from edges
+          if (itemB.xAxis < -halfWidth + minGap) {
+            itemB.xAxis = -halfWidth + minGap;
+          } else if (itemB.xAxis > halfWidth - minGap) {
+            itemB.xAxis = halfWidth - minGap;
+          }
+        }
+      }
+    }
+
+    // Ensure each item is within bounds with minGap from edges
+    if (adjustedData[i].xAxis < -halfWidth + minGap) {
+      adjustedData[i].xAxis = -halfWidth + minGap;
+    } else if (adjustedData[i].xAxis > halfWidth - minGap) {
+      adjustedData[i].xAxis = halfWidth - minGap;
+    }
+  }
+
+    return adjustedData;
+  }
 
 const getTimelineData = (timelineWidth) => {
   const timelineData = data.data;
@@ -47,7 +49,7 @@ const getTimelineData = (timelineWidth) => {
     return { ...item, yAxis, xAxis }
   })
 
-  return timelineWithAxisPositions;
+  return overlapCheck(timelineWithAxisPositions, 100, timelineWidth);
   
 }
 
